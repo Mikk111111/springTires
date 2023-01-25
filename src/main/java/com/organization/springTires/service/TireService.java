985bfe2dff5c298c7e9c99b45008d277ab8e87cd
mikk111111 <mik.k00259@gmail.com>
2023-01-25 13:16:42 +0200

    filling TireService

---
+++ src/main/java/com/organization/springTires/service/TireService.java
@@ -0,0 +1,21 @@
+package com.organization.springTires.service;
+
+import com.organization.springTires.model.Tire;
+import com.organization.springTires.repository.TireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TireService {
+
+    @Autowired
+    private TireRepository tireRepository;
+    public List<Tire> getTireModelLike(String model){
+        return tireRepository.getTireModelLike(model);
+    }
+    public List<Tire> getTireSpecsLike(String specs){
+        return tireRepository.getTireSpecsLike(specs);
+    }
+}

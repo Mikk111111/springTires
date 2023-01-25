96a6ddb874a11ac8477fa859033a9fc945edbe2f
mikk111111 <mik.k00259@gmail.com>
2023-01-25 12:03:02 +0200

    filling TireRepository

--- src/main/java/com/organization/springTires/repository/TireRepository.java
+++ src/main/java/com/organization/springTires/repository/TireRepository.java
@@ -1,4 +1,15 @@
 package com.organization.springTires.repository;
 
-public interface TireRepository {
+import com.organization.springTires.model.Tire;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TireRepository extends CrudRepository<Tire,String> {
+    @Query(value ="SELECT * FROM tires WHERE model LIKE :model",nativeQuery = true)
+    List<Tire> getTireModelLike(@Param("model") String model);
+    @Query(value ="SELECT * FROM tires WHERE modelspecs LIKE :modelSpecs",nativeQuery = true)
+    List<Tire> getTireSpecsLike(@Param("model") String modelSpecs);
 }

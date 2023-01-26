45e6f7ba6d14be25e2606b206802845f7401688c
mikk111111 <mik.k00259@gmail.com>
2023-01-26 11:21:01 +0200

    bug fixes

--- src/main/java/com/organization/springTires/repository/WarehouseRepository.java
+++ src/main/java/com/organization/springTires/repository/WarehouseRepository.java
@@ -8,18 +8,12 @@ import java.util.List;
 
 
 public interface WarehouseRepository extends CrudRepository<Warehouse,Integer> {
-    //Optional<Warehouse> findByWarehouseTireName(String tireName);
-    //Optional<Warehouse> findByWarehouseManufacturer(String manufacturer);
-    //Optional<Warehouse> findByWarehouseModel(String model);
-    //Optional<Warehouse> findByWarehousePrice(double price);
-    //Optional<Warehouse> findByWarehouseAmount(int amount);
-
     @Query(value ="SELECT * FROM warehouse WHERE tirename LIKE :name",nativeQuery = true)
-    List<Warehouse> getWarehouseTireNameLike(@Param("name") String tireName);
+    Warehouse getWarehouseTireNameLike(@Param("name") String tireName);
     @Query(value ="SELECT * FROM warehouse WHERE manufacturer LIKE :name",nativeQuery = true)
-    List<Warehouse> getWarehouseManufacturerLike(@Param("name") String manufacturer);
+    Warehouse getWarehouseManufacturerLike(@Param("name") String manufacturer);
     @Query(value ="SELECT * FROM warehouse WHERE model LIKE :model",nativeQuery = true)
-    List<Warehouse> getWarehouseModelLike(@Param("model") String model);
+    Warehouse getWarehouseModelLike(@Param("model") String model);
     @Query(value ="SELECT * FROM warehouse ORDER BY price ASC")
     List<Warehouse> getWarehousePriceASC();
     @Query(value ="SELECT * FROM warehouse ORDER BY price DESC")
